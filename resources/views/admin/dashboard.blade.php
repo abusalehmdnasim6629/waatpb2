55dead45541b6bee0fa678f5fdfd0fab2aa2fca2
unknown <sabbir.hossain2668@gmail.com>
2020-03-30 23:53:34 +0600

    dashboard change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,8 +21,8 @@
     =========================================================
     * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 	-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href="{{asset('main.css')}}" rel="stylesheet">
+	<link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="{{asset('assets/main.css')}}" rel="stylesheet">
 </head>
 
 <body>
