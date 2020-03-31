f073a1837403be4b11425a88c3bcb63a894ed9e8
unknown <sabbir.hossain2668@gmail.com>
2020-03-31 20:28:24 +0600

    admin icon problem resolve

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,18 +11,10 @@
 		content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
 	<meta name="description" content="This is an example dashboard created using build-in elements and components.">
 	<meta name="msapplication-tap-highlight" content="no">
-	<!--
-    =========================================================
-    * ArchitectUI HTML Theme Dashboard - v1.0.0
-    =========================================================
-    * Product Page: https://dashboardpack.com
-    * Copyright 2019 DashboardPack (https://dashboardpack.com)
-    * Licensed under MIT (https://github.com/DashboardPack/architectui-html-theme-free/blob/master/LICENSE)
-    =========================================================
-    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-	-->
-	<link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 	<link href="{{asset('assets/main.css')}}" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 
 <body>
@@ -87,7 +79,7 @@
 										</a>
 										<div tabindex="-1" role="menu" aria-hidden="true"
 											class="dropdown-menu dropdown-menu-right">
-										
+
 											<div tabindex="-1" class="dropdown-divider"></div>
 
 											<button onclick="event.preventDefault();
@@ -197,7 +189,7 @@
 											All History
 										</a>
 									</li>
-					
+
 								</ul>
 							</li>
 							<li>
@@ -234,7 +226,7 @@
 											All about
 										</a>
 									</li>
-									
+
 								</ul>
 							</li>
 							<li>
@@ -250,7 +242,7 @@
 											All header Content
 										</a>
 									</li>
-									
+
 								</ul>
 							</li>
 
