a911c3b0d68cf0f5f52712e5ce98d2e835dcebcd
nasim <nasim15-6629@diu.edu.bd>
2020-04-01 01:15:19 +0600

    member(admin panel same disign-done)

--- resources/views/admin/add_job.blade.php
+++ resources/views/admin/add_job.blade.php
@@ -13,95 +13,80 @@
 					<i class="icon-edit"></i>
 					<a href="#">Add job</a>
 				</li>
-			</ul>
-			
-			<div class="row-fluid sortable">
-                
-                <p class="alert-success">
-                   <?php
-                      $m = Session::get('messege');
-                      echo $m;
-                      Session::put('messege',null);
-                   
-                   
-                   ?>
-                </p>
-				<div class="box span12">
-					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon edit"></i><span class="break"></span>Add job</h2>
+</ul>
+               @if ($errors->any())
+						<div class="alert alert-danger">
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+				@endif
+	<div class="box span12">
+		<div class="box-content">
+		   
+			<div class="col-md-8 offset-md-2">
+				<form action="{{url('/save-job')}}" method="POST" enctype="multipart/form-data">
+					@csrf
+					<div class="form-group">
+						<h3>Employee Name</h3>
+						<input type="text" class="form-control" name="e_name" required="" >
 						
 					</div>
-					<div class="box-content">
-						<form class="form-horizontal" action="{{url('/save-job')}}" method="post" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-						  <fieldset>
-
-							<div class="control-group">
-							  <label class="control-label" for="date01">Employee name</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="e_name" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Department</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="department" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Position</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="position" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Vacancies</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="vacancies" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Job location</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="job_location" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Joining date</label>
-							  <div class="controls">
-								<input type="date" class="input-xlarge" name="joining_date" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Package</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="package" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-							  <label class="control-label" for="date01">Deadline</label>
-							  <div class="controls">
-								<input type="date" class="input-xlarge" name="deadline" required="" >
-							  </div>
-							</div>
-                            <div class="control-group">
-                                <label class="control-label">Additional note</label>
-                                <div class="controls">
-                                <textarea class="cleditor" name="a_note" required=" "  rows="3"></textarea>
-                                </div>
-                            </div>
-
-    	                    <br>
-							<div class="form-actions">
-							  <button type="submit" class="btn btn-primary">Add job</button>
-							  <button type="reset" class="btn">Cancel</button>
-							</div>
-						  </fieldset>
-						</form>   
-
+					<div class="form-group">
+						<h3>Department</h3>
+						<input type="text" class="form-control" name="department" required="" >
+						
 					</div>
-				</div><!--/span-->
+					<div class="form-group">
+						<h3>Position</h3>
+						<input type="text" class="form-control" name="position" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Vacancies</h3>
+						<input type="text" class="form-control" name="vacancies" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Job location</h3>
+						<input type="text" class="form-control" name="job_location" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Joining date</h3>
+						<input type="date" class="form-control" name="joining_date" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Package</h3>
+						<input type="text" class="form-control" name="package" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Deadline</h3>
+						<input type="date" class="form-control" name="deadline" required="" >
+						
+					</div>
+					<div class="form-group">
+						<h3>Additional notes</h3>
+						<textarea name="a_note" class="form-control" cols="30" rows="5" required="">
+						
+						</textarea>
+					</div>
+					
 
-			</div><!--/row-->
+					
 
+					<button type="submit" class="btn btn-info float-right">
+						 Add job
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!--/span-->
+
+</div>
 @endsection
